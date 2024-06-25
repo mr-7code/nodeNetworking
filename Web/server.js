a9e39e5991d7176d213f46524f8846e910f79127
mr7-code <morris@reutters.ch>
2024-06-25 22:03:46 +0200

    Added reRead when HTTP Requests are made (Performance and Debug considerations)

--- Web/server.js
+++ Web/server.js
@@ -1,43 +1,59 @@
+//Imports
 const http = require('http');
 const websocket = require('ws');
 
 const fs = require('fs');
 
+
+//Setup for HTTP requests
 const htmlFileName = "index.html";
 const jsFileName = "script.js";
 const cssFileName = "style.css";
 
 const clientFolder = "./Client/"
 
+/* const htmlFile = fs.readFileSync(clientFolder + htmlFileName, 'utf8');
+const jsFile = fs.readFileSync(clientFolder + jsFileName, 'utf8');
+const cssFile = fs.readFileSync(clientFolder + cssFileName, 'utf8'); */
 
-//HTTP
-const httpPORT=3000
+const reReadFiles = true;
+
+const httpPORT = 3000;
+const OKCode = 200;
+const errorCode = 404;
+
+//HTTP Server (Read Files on start or when requested?)
 const httpServer = http.createServer((req, res) => {
-
     switch(req.url){
         case '/':
-            const htmlFile = fs.readFileSync(clientFolder + htmlFileName, 'utf8');
-            res.writeHead(200, { 'Content-Type': 'text/html' });
+            if(reReadFiles){
+                htmlFile = fs.readFileSync(clientFolder + htmlFileName, 'utf8');
+            }
+            res.writeHead(OKCode, { 'Content-Type': 'text/html' });
             res.write(htmlFile);
             res.end();
             break;
 
         case '/' + jsFileName:
-            const jsFile = fs.readFileSync(clientFolder + jsFileName, 'utf8');
-            res.writeHead(200, { 'Content-Type': 'text/javascript' });
+            if(reReadFiles){
+                jsFile = fs.readFileSync(clientFolder + jsFileName, 'utf8');
+            }
+            res.writeHead(OKCode, { 'Content-Type': 'text/javascript' });
             res.write(jsFile);
             res.end();
             break;
 
         case '/' + cssFileName:
-            const cssFile = fs.readFileSync(clientFolder + cssFileName, 'utf8');
-            res.writeHead(200, { 'Content-Type': 'text/css' });
+            if(reReadFiles){
+                cssFile = fs.readFileSync(clientFolder + cssFileName, 'utf8');
+            }
+            res.writeHead(OKCode, { 'Content-Type': 'text/css' });
             res.write(cssFile);
             res.end();
             break;
 
         default:
-            res.writeHead(404, { 'Content-Type': 'text/plain' });
+            res.writeHead(errorCode, { 'Content-Type': 'text/plain' });
             res.end('404 Not Found');
             break;
     }
